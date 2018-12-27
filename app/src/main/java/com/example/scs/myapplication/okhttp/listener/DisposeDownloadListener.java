b50fb0dccdaf7218c9a7d36d35a23a12b2e99d9a
gaoyouhuang <1013172884@qq.com>
2018-12-27 17:26:49 +0800

    添加okhttp封装

--- app/src/main/java/com/example/scs/myapplication/okhttp/listener/DisposeDownloadListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.scs.myapplication.okhttp.listener;
-
-/**
- * @author vision
- * @function 监听下载进度
- */
-public interface DisposeDownloadListener extends DisposeDataListener {
-	public void onProgress(int progrss);
-}
